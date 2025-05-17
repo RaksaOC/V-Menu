3ae24d2f843f37290455c6337305e0fffdaad032
Chanraksa Ory <OCraksa@gmail.com>
2025-05-17 13:50:25 +0700

    added tenant support to customer and kitchen

--- src/app/api/kitchen/service.ts
+++
@@ -1,9 +0,0 @@
-import {Order} from "@/app/shared/model/Order";
-import {connectToDB} from "@/app/shared/lib/db";
-
-export async function fetchOrders(){
-    await connectToDB();
-    const result = await Order.find();
-    return result;
-}
-
